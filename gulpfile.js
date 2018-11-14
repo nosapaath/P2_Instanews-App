8c20506a6e1e0769ca0ea2cc2fa7cedaf4dfad18
Jon De la Mora <jon.delamora@gmail.com>
2018-11-13 18:56:30 -0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+// Proyect 2: INSTANEWS APP | Jonathan de la Mora-->
+
+// Variables
+var gulp = require("gulp"),
+    uglify = require("gulp-uglify"),
+    rename = require("gulp-rename"),
+    browserSync = require('browser-sync').create(),
+    eslint = require("gulp-eslint");
+    
+
+
+gulp.task("default", function() {
+  return gulp
+    .src("./js/*.js") // What files do we want gulp to consume?
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(uglify()) // Call the uglify function on these files
+    .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
+    .pipe(gulp.dest("./build/js")); // Where do we put the result?
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+});
+
+var sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  cssnano = require("gulp-cssnano"),
+  prettyError = require("gulp-prettyerror"),
+  rename = require("gulp-rename");
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/styles.scss")
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./build/css"))
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./build/css"));
+});
+
+gulp.task('browserSync', function () {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+
+  gulp
+  .watch(["*.html", "js/*.js", "sass/*.scss"])
+  .on("change", browserSync.reload);
+});
---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+// Proyect 2: INSTANEWS APP | Jonathan de la Mora-->
+
+$(function(){   //General API call
+
+	
+	$('#selectBtn').on("change", function() {      //Change menu option
+			$("#newsGallery").empty();
+			console.log(newsGallery);
+
+		var newsCategory = event.target.value;
+
+		$('#loadingAjax').show();
+
+		$.ajax({																	 //Get information from specific category
+		  method: "GET",
+		  url: 'https://api.nytimes.com/svc/topstories/v2/' + newsCategory + '.json?' + 
+			$.param({"api-key": "b76abdc620b044a8aeec75d713dcdb0f"})
+		})
+
+		.done(function(data) {
+			console.log(data.results);
+			var count = 0;
+			$.each(data.results, function(index, data){ 	//Post individual title and image		
+				// console.log(data.title);
+				if(data.multimedia[4] && count < 12){
+					count++;
+						$('#newsGallery').append(
+						'<a href="'+ data.url +'" target="_blank"><li style="background-image:url('+ data.multimedia[4].url +')"><p>'+ data.title +'</p></li></a>'
+						);
+				}
+						$('#loadingAjax').hide();
+			});
+		});		
+	});
+});
